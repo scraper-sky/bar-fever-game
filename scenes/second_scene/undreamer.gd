66c075f6a77ae65e364861986f19bde5dc9464e2
Jerry Xiao <jerryxiao@Jerrys-MacBook-Pro.local>
2025-02-11 23:46:28 -0800

    Modified dialoge and added an undreaming device

---
+++ scenes/second_scene/undreamer.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_undream_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/third_scene/world_3.tscn")
+
+func _on_exit_pressed() -> void:
+	self.hide()
---
+++ scenes/second_scene/world_2.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+# You need to reference the node first
+@onready var undreamer = $Undreamer  # Assuming "Undreamer" is a child node
+# Can't have code outside functions
+func _ready():
+	undreamer.hide()
+
+func _process(delta):
+	if Input.is_action_pressed("undream"):
+		undreamer.show()
