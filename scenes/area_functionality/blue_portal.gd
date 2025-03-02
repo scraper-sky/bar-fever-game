1bbf23154d34f3e5453b8338169b2ec47dc4ee19
Jerry Xiao <jxiao45@stanford.edu>
2025-03-02 15:52:19 -0800

    added flickering portal

--- scenes/area_functionality/blue_portal.gd
+++ scenes/area_functionality/blue_portal.gd
@@ -1,18 +1,17 @@
 extends Area2D
 
-# Export variable to set the target scene in the editor
 @export var target_scene: String = "res://scenes/levels/level_2.tscn"
 var fade: ColorRect
 
 func _ready():
 	body_entered.connect(_on_body_entered)
-	# Pre-create fade node
+	$AnimatedSprite2D.play("flicker")  
 	fade = ColorRect.new()
 	fade.color = Color(0, 0, 0, 0)
 	var screen_size = get_viewport().get_visible_rect().size
 	fade.set_size(screen_size)
 	fade.position = -screen_size / 2
-	add_child(fade)  # Add to BluePortal, not root
+	add_child(fade)  
 
 func _on_body_entered(body):
 	if body.name == "MainPlayer":
