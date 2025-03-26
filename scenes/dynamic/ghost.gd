94204a95399afd4e347dea1ae5f08941d8dd90d1
Jerry Xiao <jxiao45@stanford.edu>
2025-03-26 04:21:56 -0700

    Pre 4.4 backup + level 7 platforming climber

--- scenes/dynamic/ghost.gd
+++ scenes/dynamic/ghost.gd
@@ -1,12 +1,15 @@
 extends Area2D
 
-@export var speed: float = 100.0 
-@export var distance: float = 128.0  
-var direction = 1 
+@export var speed: float = 100.0
+@export var distance: float = 128.0
+@export var start_direction: int = 1
+
+var direction: int
 var start_pos: Vector2
 
 func _ready():
-	start_pos = position  
+	start_pos = position
+	direction = start_direction
 	if has_node("AnimatedSprite2D"):
 		$AnimatedSprite2D.play("float")
 	body_entered.connect(_on_body_entered)
@@ -14,7 +17,7 @@ func _ready():
 func _process(delta):
 	position.x += speed * direction * delta
 	if abs(position.x - start_pos.x) >= distance:
-		direction *= -1 
+		direction *= -1
 
 func _on_body_entered(body):
 	if body.name == "MainPlayer":
