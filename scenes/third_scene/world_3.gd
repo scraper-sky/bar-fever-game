cb57c0eb576da8038d402206ddeb56ebecfaa48f
Jerry Xiao <jxiao45@stanford.edu>
2025-04-24 03:20:37 -0700

    Barfever dialogue has wave effects as character realizes purgatory; transition scenes coming next; character panels added in dialogue

--- scenes/third_scene/world_3.gd
+++ scenes/third_scene/world_3.gd
@@ -1,1 +1,7 @@
 extends Node2D
+
+@export var dialogue_start: String = "start"
+var dialogue_resource = preload("res://dialogue/curiosity.dialogue")
+
+func _ready() -> void:
+	DialogueManager.show_dialogue_balloon(dialogue_resource, dialogue_start)
